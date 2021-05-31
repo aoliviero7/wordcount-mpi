d143c5fee48c551b4a38aee4322b94368c324131
Alessandro Oliviero <a.oliviero7@gmail.com>
2021-05-31 09:03:56 -0700

    start to pack struct & bug fix

--- prova4_accenti.c
+++ prova4_accenti.c
@@ -6,6 +6,9 @@
 #include <time.h>
 #include <dirent.h>
 #include <ctype.h>
+#include <wchar.h>
+#include <wctype.h>
+#include <locale.h>
 
 #define FOLDER "txt/try"
 
@@ -15,13 +18,14 @@ typedef struct{
 }fileStats;
 
 typedef struct{
-	char parola[100];
+	wchar_t parola[100];
 	int count;
 }Word;
 
-Word* wordCount(int inizio, int inizioFile, int fineFile, int resto, int byteProcess, fileStats* myFiles, int myrank, int *size);
-Word* chunkAndCount(int myrank, int totalByte, fileStats* myFiles, int countFiles, int p, int* size);
-int getWord(FILE *file, char* str);
+void wordCount(int inizio, int inizioFile, int fineFile, int resto, int byteProcess, fileStats* myFiles, int myrank);
+void chunkAndCount(int myrank, int totalByte, fileStats* myFiles, int countFiles, int p);
+int getWord(FILE *file, wchar_t* str);
+int isAccented(wint_t c);
 
 int main(int argc, char** argv) {
 	MPI_Init(NULL, NULL);
@@ -29,6 +33,7 @@ int main(int argc, char** argv) {
     MPI_Status status;
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
+	setlocale(LC_CTYPE, "it_IT.utf8");
 
 	DIR *directory;
 	FILE *file;
@@ -69,34 +74,10 @@ int main(int argc, char** argv) {
 	else
 		printf("Directory non leggibile\n");
 	closedir(directory);
-	int size = 0;
-	Word *words = chunkAndCount(myrank, totalByte, myFiles, countFiles, p, &size);
-	for(int i=0; i<size; i++)
-		printf("Parola: %s - counts: %d\n",words[i].parola,words[i].count);
 	
-
-	MPI_Datatype Wordtype, oldtypes[2], parolatype;   	//variabili richieste per l'invio
-    int blockcounts[2];
-    MPI_Aint offsets[2], lb, extent;
-	MPI_Type_contiguous(100, MPI_CHAR, &parolatype);
-    MPI_Type_commit(&parolatype);
-    offsets[0] = 0;										//setup parola
-    oldtypes[0] = parolatype;
-    blockcounts[0] = 1;
-    MPI_Type_get_extent(parolatype, &lb, &extent);		//setup count
-    offsets[1] = 1 * extent;
-    oldtypes[1] = MPI_INT;
-    blockcounts[1] = 2;
-    MPI_Type_create_struct(2, blockcounts, offsets, oldtypes, &Wordtype);	//definizione struttura
-    MPI_Type_commit(&Wordtype);
-	int typeSize;
-    MPI_Type_size(Wordtype, &typeSize);					//dimensione tipo
-    //printf("size %d\n",size);
-    
-
-
-
-	free(words);
+	chunkAndCount(myrank, totalByte, myFiles, countFiles, p);
+	
+	
 	free(myFiles);
 	MPI_Finalize();
 }
@@ -107,7 +88,7 @@ int main(int argc, char** argv) {
 	Es: il processo 3 lavora dal byte 400 del file 5 fino al byte 150 del file 7
 	Dopodiché viene chiamato il metodo wordCount
 */
-Word* chunkAndCount(int myrank, int totalByte, fileStats* myFiles, int countFiles, int p, int *size){
+void chunkAndCount(int myrank, int totalByte, fileStats* myFiles, int countFiles, int p){
 	int resto = totalByte % p;
 	int byteProcess = totalByte / p;																		
 	int inizio = (myrank<resto) ? (byteProcess + 1) * myrank : resto + (byteProcess * myrank);				//da che byte parto (riferito ai byte totali da elaborare)
@@ -132,18 +113,18 @@ Word* chunkAndCount(int myrank, int totalByte, fileStats* myFiles, int countFile
 	}
 	printf("Processore: %d, File inizio: %d, Byte: %d - File fine: %d, Byte: %d\n",myrank,inizioFile,inizio,fineFile,fine);
 	
-	return wordCount(inizio, inizioFile, fineFile, resto, byteProcess, myFiles, myrank, size);
+	wordCount(inizio, inizioFile, fineFile, resto, byteProcess, myFiles, myrank);
 }
 
 
 /*
 	Qui ogni processo si calcola la frequenza delle parole delle porzioni dei file che gli spettano
 */
-Word* wordCount(int inizio, int inizioFile, int fineFile, int resto, int byteProcess, fileStats* myFiles, int myrank, int* size){
+void wordCount(int inizio, int inizioFile, int fineFile, int resto, int byteProcess, fileStats* myFiles, int myrank){
 	DIR *directory;
 	FILE *file;
 	struct dirent *Dirent;
-	int countByte = 0;
+	int size = 0, countByte = 0;
 	Word* words;
 	words = (Word*) malloc(sizeof(Word));
 	byteProcess = (myrank<resto) ? byteProcess + 1 : byteProcess;
@@ -161,19 +142,19 @@ Word* wordCount(int inizio, int inizioFile, int fineFile, int resto, int bytePro
 					fseek(file, inizio, SEEK_SET);				//mi posiziono al primo byte di lettura se è il primo file da leggere										
 				while(!feof(file)){								//scansiono il file
 					if(countByte<byteProcess){					//non devo leggere di più di quanto mi tocca
-						char str[100]={};
+						wchar_t str[100]={};
 						int prima = ftell(file);
-						if(!getWord(file, str))					//prendo una parola	
-							continue;																
+						if(!getWord(file, str))					//prendo una parola
+							break;											
 						int dopo = ftell(file);
 						//printf("Stringa letta: %ls \n",str);
 						int salto = 0;												//flag che mi indica se la parola la devo saltare	
 						if(iFile == inizioFile && prima == inizio && inizio!=0){	//se sto leggendo la mia prima parola (prima==inizio) del primo file che mi è stato assegnato (iFile==inizioFile),
 																					//e non mi trovo all'inizio del file (inizio!=0) (NB: rank 0 non lo farà mai dato che il suo inizio=0)
 							fseek(file, inizio-1, SEEK_SET);						//mi sposto di un byte(carattere) indietro
-							char c;
-							c = fgetc(file);										//leggo il carattere precedente a quello che mi spetta realmente	
-							if(!isspace(c))											//se il carattere precedente non è uno spazio (quindi fa parte di una parola)
+							wint_t c;
+							c = fgetwc(file);										//leggo il carattere precedente a quello che mi spetta realmente	
+							if(!iswspace(c))										//se il carattere precedente non è uno spazio (quindi fa parte di una parola)
 								salto = 1;											//devo saltare la parola poiché è già stata letta dal processo precedente, altrimenti devo leggerla	
 							fseek(file, dopo, SEEK_SET);							//mi riposiziono in modo giusto	
 						}															
@@ -181,20 +162,23 @@ Word* wordCount(int inizio, int inizioFile, int fineFile, int resto, int bytePro
 						if(salto)								//se non devo leggere la parola
 							continue;							//vado alla prossima iterazione perché la parola è già stata analizzata
 						
+						
+							
 						int flag = 0, i = 0;
-						for(i=0; i<*size; i++){					//cerco nell'array se è già presente la parola
-							if(!strcmp(str, words[i].parola)){	//se le parole sono uguali
+						for(i=0; i<size; i++){					//cerco nell'array se è già presente la parola
+							if(!wcscmp(str, words[i].parola)){	//se le parole sono uguali
 								flag = 1;						//setto il flag a true
 								break;							
 							}
 						}
-						if(flag)								//flag = 1
+						if(flag){								//flag = 1
 							words[i].count++;					//la parola esiste già e incremento il suo contatore
+						}
 						else{									//flag = 0
-							*size=*size+1;								//incremento la size e aggiungo gli elementi agli array
-							words = (Word*) realloc(words, (*size) * sizeof(Word));
-							strcpy(words[*size-1].parola, str);
-							words[*size-1].count = 1;
+							size++;								//incremento la size e aggiungo gli elementi agli array
+							words = (Word*) realloc(words, (size) * sizeof(Word));
+							wcscpy(words[size-1].parola, str);
+							words[size-1].count = 1;
 						}
 					}else
 						break;
@@ -204,12 +188,12 @@ Word* wordCount(int inizio, int inizioFile, int fineFile, int resto, int bytePro
 		}
 		MPI_Barrier(MPI_COMM_WORLD);
 		sleep(myrank);
-		printf("Processo: %d \n",myrank);
-		/*for(int i=0; i<*size; i++){
-			printf("Parola: %s - counts: %d\n",words[i].parola,words[i].count);
+		/*printf("Processo: %d \n",myrank);
+		for(int i=0; i<size; i++){
+			printf("Parola: %ls - counts: %d\n",words[i].parola,words[i].count);
 		}*/
-		printf("Processo: %d - dimensione struct: %ld\n",myrank,(*size) * sizeof(Word));
-		return words;
+		printf("Processo: %d - dimensione struct: %ld\n",myrank,(size) * sizeof(Word));
+		free(words);
 	}
 }
 
@@ -220,17 +204,21 @@ Word* wordCount(int inizio, int inizioFile, int fineFile, int resto, int bytePro
 	è possibile analizzare in input anche file di testo relativi a libri o con segni di 
 	punteggiatura tra le parole.
 */
-int getWord(FILE *file, char* str){
+int getWord(FILE *file, wchar_t* str){
 	int i=0;
-	char c;
+	wint_t c;
 	do{
-		c = fgetc(file);
-		printf("%c ",c);
-		if(isalnum(c))
-			str[i++] = tolower(c);
-	}while(isalnum(c));	
+		c = fgetwc(file);
+		//printf("%lc ",c);
+		if(iswalnum(c) || isAccented(c))
+			str[i++] = c;
+	}while(iswalnum(c) || isAccented(c));	
 	str[i] = '\0';
 	return i;
+}
+
+int isAccented(wint_t c){
+	return (c==L'è' || c==L'é' || c==L'ù' || c==L'ò' || c==L'ì' || c==L'à');
 }
 
 
